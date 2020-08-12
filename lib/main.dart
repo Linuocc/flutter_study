163924ac3fe96064c35e550b9f87a21c35d52b52
Linuocc <2953828249@qq.com>
2020-08-12 22:39:11 +0800

    StatefulWidget的生命周期

--- lib/main.dart
+++ lib/main.dart
@@ -18,20 +18,47 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class ContentWidget extends StatelessWidget {
+class ContentWidget extends StatefulWidget {
+  ContentWidget(){
+    print("ContentWidget的构造函数被调用");
+  }
   @override
-  Widget build(BuildContext context) {
-    return Center(child: TextWidget());
+  State<StatefulWidget> createState() {
+    print("ContentWidget的createState被调用");
+    return ContentState();
   }
 }
 
-class TextWidget extends StatelessWidget {
+class ContentState extends State<ContentWidget> {
+  ContentState(){
+    print("ContentState的构造函数被调用");
+  }
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    print("initState被调用");
+  }
+  @override
+  void didChangeDependencies() {
+    // TODO: implement didChangeDependencies
+    super.didChangeDependencies();
+    print("ContentState的didChangeDependencies被调用");
+  }
+  @override
+  void didUpdateWidget(ContentWidget oldWidget) {
+    // TODO: implement didUpdateWidget
+    super.didUpdateWidget(oldWidget);
+    print("ContentState的didUpdateWidget被调用");
+  }
   @override
   Widget build(BuildContext context) {
-    return Text(
-      "Hello World",
-      textDirection: TextDirection.ltr,
-      style: TextStyle(fontSize: 30, color: Colors.orange),
+    print("build被调用");
+    return Center(
+      child: Text(
+        "Hello",
+        style: TextStyle(fontSize: 30),
+      ),
     );
   }
 }
